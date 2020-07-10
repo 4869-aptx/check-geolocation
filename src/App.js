1db183d3cc51402a23adf3498327ce4c34c90ab2
Prankul Singh <prankul.singh@airtel.com>
2020-07-10 17:28:44 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Location from "./location";
+
+function App() {
+  return (
+      <Location/>
+  );
+}
+
+export default App;
